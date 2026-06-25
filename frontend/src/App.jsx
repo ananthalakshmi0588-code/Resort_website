c53eef9102c614f9890f87637cba8f65777af865
ananthalakshmi0588 <ananthalakshmi0588@gmail.com>
2026-06-25 14:58:41 +0530

    install missing package

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -66,7 +66,8 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar"
 import Home from './components/Home';
 import RoomsPage from './components/RoomsPage';
 import Gallery from './components/Gallery';
